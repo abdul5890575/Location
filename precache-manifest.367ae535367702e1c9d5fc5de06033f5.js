bbed1663b5de7353652b435dd6a6266a3e520523
Abdul Rehman Shaukat <abdulrehman.shoukat@gmail.com>
2020-11-16 20:10:06 -0500

    Updates

---
+++ precache-manifest.367ae535367702e1c9d5fc5de06033f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1fa22feb099ccbf428f475d83b14e56",
+    "url": "/Location/index.html"
+  },
+  {
+    "revision": "7392689e64e1c0285a4a",
+    "url": "/Location/static/css/main.e1f1f93d.chunk.css"
+  },
+  {
+    "revision": "5999bb4d3ea9f6a79b60",
+    "url": "/Location/static/js/2.30223bd1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Location/static/js/2.30223bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7392689e64e1c0285a4a",
+    "url": "/Location/static/js/main.c7641d33.chunk.js"
+  },
+  {
+    "revision": "9eba2acd2d6be1b22619",
+    "url": "/Location/static/js/runtime-main.fda22dd2.js"
+  }
+]);
